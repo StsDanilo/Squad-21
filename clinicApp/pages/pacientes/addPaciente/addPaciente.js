d57ba72b0edcea6fcb171c92c83b83f635bd9235
DaniloSts <dsb.danilo07@gmail.com>
2025-09-06 15:59:59 -0300

    Máscaras implementadas

--- clinicApp/pages/pacientes/addPaciente/addPaciente.js
+++ clinicApp/pages/pacientes/addPaciente/addPaciente.js
@@ -18,8 +18,53 @@ document.addEventListener("DOMContentLoaded", () => {
                 document.getElementById("celular").value = pacienteParaEditar.celular;
                 document.getElementById("cidade").value = pacienteParaEditar.cidade;
                 document.getElementById("estado").value = pacienteParaEditar.estado;
+                document.getElementById("nomeSocial").value = pacienteParaEditar.nomeSocial;
+                document.getElementById("cpf").value = pacienteParaEditar.cpf;
+                document.getElementById("rg").value = pacienteParaEditar.rg;
+                document.getElementById("outrosDocs").value = pacienteParaEditar.outrosDocs;
+                document.getElementById("numDoc").value = pacienteParaEditar.numDoc;
+                document.getElementById("sexo").value = pacienteParaEditar.sexo;
+                document.getElementById("dataNascimento").value = pacienteParaEditar.dataNascimento;
+                document.getElementById("etnia").value = pacienteParaEditar.etnia;
+                document.getElementById("raca").value = pacienteParaEditar.raca;
+                document.getElementById("nacionalidade").value = pacienteParaEditar.nacionalidade;
+                document.getElementById("estadoCivil").value = pacienteParaEditar.estadoCivil;
+                document.getElementById("nomeMae").value = pacienteParaEditar.nomeMae;
+                document.getElementById("profissaoMae").value = pacienteParaEditar.profissaoMae;
+                document.getElementById("nomePai").value = pacienteParaEditar.nomePai;
+                document.getElementById("profissaoPai").value = pacienteParaEditar.profissaoPai;
+                document.getElementById("nomeResponsavel").value = pacienteParaEditar.nomeResponsavel;
+                document.getElementById("cpfResponsavel").value = pacienteParaEditar.cpfResponsavel;
+                document.getElementById("nomeEsposo").value = pacienteParaEditar.nomeEsposo;
+                document.getElementById("endereco").value = pacienteParaEditar.endereco;
+                document.getElementById("numero").value = pacienteParaEditar.numero;
+                document.getElementById("complemento").value = pacienteParaEditar.complemento;
+                document.getElementById("bairro").value = pacienteParaEditar.bairro;
+                document.getElementById("email").value = pacienteParaEditar.email;
+                document.getElementById("telefone1").value = pacienteParaEditar.telefone1;
+                document.getElementById("tipoSanguineo").value = pacienteParaEditar.tipoSanguineo;
+                document.getElementById("peso").value = pacienteParaEditar.peso;
+                document.getElementById("altura").value = pacienteParaEditar.altura;
+                document.getElementById("alergias").value = pacienteParaEditar.alergias;
+                document.getElementById("convenio").value = pacienteParaEditar.convenio;
+                document.getElementById("plano").value = pacienteParaEditar.plano;
+                document.getElementById("matricula").value = pacienteParaEditar.matricula;
+                document.getElementById("validadeCarteira").value = pacienteParaEditar.validadeCarteira;
+                document.getElementById("validadeIndeterminada").value = pacienteParaEditar.validadeIndeterminada;
+                document.getElementById("codigoLegado").value = pacienteParaEditar.codigoLegado;
+                document.getElementById("observacoes").value = pacienteParaEditar.observacoes;
+                document.getElementById("anexos").value = pacienteParaEditar.anexos;
             }
         }
+
+        const cpf = document.getElementById("cpf");
+        const telefone = document.getElementById("telefone1");
+        const celular = document.getElementById("celular");
+
+        // Aplique as máscaras
+        new Inputmask("999.999.999-99").mask(cpf);
+        new Inputmask("(99) 99999-9999").mask(celular);
+        new Inputmask("(99) 9999-9999").mask(telefone);
 
         form.addEventListener("submit", (e) => {
             e.preventDefault();
@@ -30,6 +75,43 @@ document.addEventListener("DOMContentLoaded", () => {
                 celular: document.getElementById("celular").value,
                 cidade: document.getElementById("cidade").value,
                 estado: document.getElementById("estado").value,
+                nomeSocial: document.getElementById("nomeSocial").value,
+                cpf: document.getElementById("cpf").value,
+                rg: document.getElementById("rg").value,
+                outrosDocs: document.getElementById("outrosDocs").value,
+                numDoc: document.getElementById("numDoc").value,
+                sexo: document.getElementById("sexo").value,
+                dataNascimento: document.getElementById("dataNascimento").value,
+                etnia: document.getElementById("etnia").value,
+                raca: document.getElementById("raca").value,
+                nacionalidade: document.getElementById("nacionalidade").value,
+                estadoCivil: document.getElementById("estadoCivil").value,
+                nomeMae: document.getElementById("nomeMae").value,
+                profissaoMae: document.getElementById("profissaoMae").value,
+                nomePai: document.getElementById("nomePai").value,
+                profissaoPai: document.getElementById("profissaoPai").value,
+                nomeResponsavel: document.getElementById("nomeResponsavel").value,
+                cpfResponsavel: document.getElementById("cpfResponsavel").value,
+                nomeEsposo: document.getElementById("nomeEsposo").value,
+                endereco: document.getElementById("endereco").value,
+                numero: document.getElementById("numero").value,
+                complemento: document.getElementById("complemento").value,
+                bairro: document.getElementById("bairro").value,
+                email: document.getElementById("email").value,
+                celular: document.getElementById("celular").value,
+                telefone1: document.getElementById("telefone1").value,
+                tipoSanguineo: document.getElementById("tipoSanguineo").value,
+                peso: document.getElementById("peso").value,
+                altura: document.getElementById("altura").value,
+                alergias: document.getElementById("alergias").value,
+                convenio: document.getElementById("convenio").value,
+                plano: document.getElementById("plano").value,
+                matricula: document.getElementById("matricula").value,
+                validadeCarteira: document.getElementById("validadeCarteira").value,
+                validadeIndeterminada: document.getElementById("validadeIndeterminada").value,
+                codigoLegado: document.getElementById("codigoLegado").value,
+                observacoes: document.getElementById("observacoes").value,
+                anexos: document.getElementById("anexos").value,
             };
 
             if (pacienteParaEditar) {
